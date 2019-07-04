46b00357e397c93abca58a939868d15ed2460e6c
Ionut Dincu <gabrieldincu@outlook.com>
2019-07-04 11:08:42 +0100

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -5,3 +5,10 @@ while True:
     sys.stdout.write("%s\r" % i)
     sys.stdout.flush()
     time.sleep(.05)
+    
+#Print in one line dynamically
+"""
+tempName = str(input("Nume: "))
+for i in range(0,len(tempName),1):
+  print(tempName[i], sep=' ', end='', flush=True)
+"""
