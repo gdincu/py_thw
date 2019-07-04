c9b3295ae01c2af6d7c1f84523a611b4bc864f3f
Ionut Dincu <gabrieldincu@outlook.com>
2019-07-04 09:38:12 +0300

    Add files via upload

--- ex1.py
+++ ex1.py
@@ -23,6 +23,7 @@ z='15'
 print("x+y+z = " + str((x+y+int(z))))
 x=y=z
 prints(x,y,z)
+print("X would be %d, Y is %s and Z is %r" %(x, str(y), z))
 """
 
 #Numbers
@@ -38,7 +39,7 @@ y = 2.8 # float
 #convert from float to int:
 b = int(y)
 
-z = 1j # complex
+z = 2j # complex
 #convert from int to complex:
 c = complex(x)
 """
@@ -81,6 +82,12 @@ myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
 print(myorder.format(quantity, itemno, price))
 """
 
+#Formatter
+"""
+formatter = "%r %d %s"
+print (formatter % (1,2,'3'))
+"""
+
 #Operators
 """
 print(5%3)
@@ -93,4 +100,23 @@ print (x is 3)
 print (x is not 3)
 x = ["apple", "banana"]
 print("pineapple" not in x)
+"""
+
+#Escape sequences
+"""
+\\ -> \
+\' -> '
+\" -> "
+\a -> ASCII bell -> Bell character is an ASCII control character, code 7 (^G). When it is sent to a printer or a terminal, nothing is printed, but an audible signal is emitted instead. You can emit a beep by printing the ASCII Bell character ("\0007") to the console.
+\b -> Backspace
+\f -> formfeed
+\n -> linefeed
+\N{name} -> char named name in the unicode database
+\r -> carriage return
+\t -> horizontal tab
+\uxxx -> char with 16-bit hex value xxxx
+\Uxxxxxxxx -> char with 32-bit hex value xxxxxxxx
+\v -> vertical tab
+\ooo -> char with octal value ooo
+\xhh -> char with hex value hh
 """
