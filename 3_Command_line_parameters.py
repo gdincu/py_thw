fff48f2a271279992779c73c364a9a67b0fe75f8
Ionut Dincu <gabrieldincu@outlook.com>
2019-07-05 11:56:19 +0300

    Update 3_Command_line_parameters.py

--- 3_Command_line_parameters.py
+++ 3_Command_line_parameters.py
@@ -10,3 +10,9 @@ print("The arguments are: ",str(sys.argv))
 if len(sys.argv) > 1:
 	for i in range(1,len(sys.argv),1):
 		print("Argument %d: " %i,sys.argv[i])
+		
+"""
+from sys import argv
+name,script,input_file = argv
+print(script,input_file)
+"""
